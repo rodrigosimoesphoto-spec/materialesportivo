dff24d0c56301db5fc6c460fec743880f558b5e3
rodrigosimoesphoto-spec <rodrigosimoes.photo@gmail.com>
2026-07-29 21:27:51 -0300

    Remover versão PWA instável

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE='sigme-cleanup-rev1';
-self.addEventListener('install',event=>self.skipWaiting());
-self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',()=>{});
